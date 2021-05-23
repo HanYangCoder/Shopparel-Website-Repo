8bb55632070f78524abac409d94fc7785195ce3c
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-05-23 22:43:11 +0800

    last test commit

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
   // note to self, specify what folder is your connection located at
   require("testFiles/db_connection.php");
   $myName = "Hans";
-  $role = "developer";
+  $role = "coder";
   echo "Hello World! My name is $myName and I am a $role! Welcome!";
 ?>

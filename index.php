d4d7e0ee096061bbfc4dcc77cf9c3929dab7830e
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-05-23 21:49:27 +0800

    Edited index.php for test only

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
   // note to self, specify what folder is your connection located at
   require("testFiles/db_connection.php");
   $myName = "Hans";
-  echo "Hello World! My name is $myName and I am a developer";
+  echo "Hello World! My name is $myName and I am a developer!";
 ?>

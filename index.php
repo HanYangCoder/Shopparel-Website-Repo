06bf202fa00eac2e23044ddfe9749692de834bd7
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-05-23 22:04:12 +0800

    test commit

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
   // note to self, specify what folder is your connection located at
   require("testFiles/db_connection.php");
   $myName = "Hans";
-  echo "Hello World! My name is $myName and I am a developer!";
+  $role = "developer";
+  echo "Hello World! My name is $myName and I am a $role!";
 ?>

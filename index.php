6ede73cadddc604b8829b446b5dc8b97a9ccf097
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-05-23 23:21:09 +0800

    Final test commit

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
   require("testFiles/db_connection.php");
   $myName = "Hans";
   $role = "coder";
-  echo "Hello World! My name is $myName and I am a $role! Welcome!";
+  echo "Hello World! My name is $myName and I am a $role! Welcome to the land  of the brave open-source devs!";
 ?>

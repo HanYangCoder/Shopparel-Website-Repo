8f2a2b594adb032d27f2d2b0aa6ccf4eab9e0b2d
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-05-24 09:30:27 +0800

    Testing PHP redirection

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
   // note to self, specify what folder is your connection located at
   require("testFiles/db_connection.php");
-  $myName = "Hans";
-  $role = "coder";
-  echo "Hello World! My name is $myName and I am a $role! Welcome to the land  of the brave open-source devs!";
+  
+  header ("Location: testFiles/showAll.php");
 ?>

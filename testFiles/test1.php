e8d5172513dd7518c3c43ba11082cc58537164f4
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-05-24 14:48:17 +0800

    Yahoo yay test commit

--- testFiles/test1.php
+++ testFiles/test1.php
@@ -1,5 +1,5 @@
 <?php
   $myName = "Hans";
   $role = "coder";
-  echo "Hello World! My name is $myName and I am a $role! Welcome to the land  of the brave open-source devs! Yahooooo";
+  echo "Hello World! My name is $myName and I am a $role! Welcome to the land  of the brave open-source devs! Yaayyyy";
 ?>
